db3efd5fcd409da9bde0ccbe5c294c334ea51c5e
cloudsfrontier <65422730+cloudsfrontier@users.noreply.github.com>
2020-05-20 23:04:29 -0700

    CachePolling

--- CacheCounter/force-app/main/default/lwc/taskCounter/taskCounter.js
+++ CacheCounter/force-app/main/default/lwc/taskCounter/taskCounter.js
@@ -1,20 +1,46 @@
-import { LightningElement,wire,track } from 'lwc';
+import { LightningElement,wire,track, api } from 'lwc';
 import getOpenTaskCount from '@salesforce/apex/taskCounter.getOpenTaskCount';
+import {refreshApex} from '@salesforce/apex';
 
 export default class taskCounter extends LightningElement {
 counter;
 error;
 
 
-  @wire(getOpenTaskCount) wiredCounter
+/*  @wire(getOpenTaskCount) wiredCounter
   ({ error, data }) {
     if (data) {
         this.counter = data;
     } else if (error) {
         console.log(error);
         this.error = error;
-    } ;
+    } ;*/
+
+    wiredMessages;  
+
+    @wire(getOpenTaskCount) 
+    wiredCounter(value) {
+        
+          this.wiredMessages = value;
+          const {data, error} = value;
+
+          if(data){
+            this.counter  = data;
+            setInterval(()=>{  
+              console.log('in SI');
+    
+              return refreshApex(this.wiredMessages);  
+                
+            }, 1000);
+          }
+    
+         else if (error) {
+            console.log(error);
+            this.error = error;
+           
+        } ;
+      }
   
  
-}
+
 }
